077166dbd9e27a80e34a605a4508d21a904c5cbc
Cgotts <cobi.gottschalk@gmail.com>
2022-12-11 09:55:09 -0800

    Got the data to show in the console, added post request, adjusted file routes, not sure if fsUtils is doing anything

---
+++ server.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const path = require('path');
+
+const api = require('./routes/notes.js');
+// const notesApi = require('./routes/notes.js');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
+// app.use('/api', notesApi);
+
+app.use(express.static('public'));
+
+// GET Route for homepage
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+// GET Route for feedback page
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+
+
+
+// POST request to add a review
+app.post('/api/notes', (req, res) => {
+  // Log that a POST request was received
+  console.info(`${req.method} request received to add a review`);
+  // Prepare a response object to send back to the client
+  let response;
+
+  // Check if there is anything in the response body
+  if (req.body && req.body.product) {
+    response = {
+      status: 'success',
+      data: req.body,
+    };
+    res.status(201).json(response);
+  } else {
+    res.status(400).json('Request body must at least contain a note name');
+  }
+
+  // Log the response body to the console
+  console.log(req.body);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
+
